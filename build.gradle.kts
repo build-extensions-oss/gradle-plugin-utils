a94090a0c76d0a4afa2ffc6724af38d091028a27
Igor Manushin <igor.manushin@pm.me>
2025-12-05 21:15:22 +0000

    depend build on report

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,21 @@
 plugins {
+    base
     alias(libs.plugins.org.jetbrains.kotlinx.kover)
+}
+
+dependencies {
+    subprojects.forEach {
+        val projectName = it.name
+
+        // depend on all projects
+        if (projectName != "bom") {
+            kover(project(":${it.name}")) {}
+        }
+    }
+}
+
+tasks {
+    this.getByName("build") {
+        dependsOn(getByName("koverXmlReport"))
+    }
 }
