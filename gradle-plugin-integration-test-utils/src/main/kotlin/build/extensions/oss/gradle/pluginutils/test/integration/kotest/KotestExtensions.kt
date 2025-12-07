3dc5322cec79ff6c7cfa1e93593ce3c7764e678c
Igor Manushin <igor.manushin@pm.me>
2025-12-07 10:52:49 +0000

    move files to project matching package

---
+++ gradle-plugin-integration-test-utils/src/main/kotlin/build/extensions/oss/gradle/pluginutils/test/integration/kotest/KotestExtensions.kt
@@ -0,0 +1,14 @@
+package build.extensions.oss.gradle.pluginutils.test.integration.kotest
+
+import io.kotest.core.TestConfiguration
+import build.extensions.oss.gradle.pluginutils.test.integration.TestProject
+
+
+/**
+ * Creates a [TestProject] with the given name and registers it to be closed after
+ * the spec is completed.
+ *
+ * @param name the name of the project
+ */
+fun TestConfiguration.testProject(name: String = "test-project") =
+    autoClose(TestProject(name))
